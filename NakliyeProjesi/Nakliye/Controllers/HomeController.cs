b4fca501ea4768f4652ebea36d859ef329cebb62
Gülbahar İnan <glbaharinan.47@gmail.com>
2019-04-28 19:19:29 +0300

    İlk Checkin

---
+++ NakliyeProjesi/Nakliye/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nakliye.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
