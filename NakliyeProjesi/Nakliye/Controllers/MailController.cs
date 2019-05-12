c2b4589ea0b07027ac7488b68265a520d415b842
büsra başaran <busrab92@hotmail.com>
2019-05-12 16:59:20 +0300

    Mail gönderimi yapıldı

---
+++ NakliyeProjesi/Nakliye/Controllers/MailController.cs
@@ -0,0 +1,37 @@
+﻿using Nakliye.BLL;
+using Nakliye.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Nakliye.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = new StringBuilder();
+                body.AppendLine("Ad & Soyad: " + model.Name);
+                body.AppendLine("E-Mail Adresi: " + model.Email);
+                body.AppendLine("Konu: " + model.Subject);
+                body.AppendLine("Mesaj: " + model.Message);
+                MailBLL.MailSender(body.ToString());
+            }
+            return View();
+        }
+
+    }
+}
